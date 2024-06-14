171ffca55418998ad43e4352a3993987bbff7e3d
izazahmed0177 <izazahmed0177@gmail.com>
2024-06-14 13:17:59 +0600

    produt root add

--- index.js
+++ index.js
@@ -2,12 +2,64 @@
 const express=require("express")
 const app=express();
 const cors=require("cors");
+
+var jwt = require('jsonwebtoken');
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port=5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+// jwt use ===========
+
+
+function createToken(user) {
+
+    const token = jwt.sign(
+      {
+        email:user.email
+      },
+  
+      "secret",
+  
+      { expiresIn: "7d" }
+    );
+  
+    return token;
+  }
+
+
+  //------------
+  
+  
+  function verifyToken(req,res,next) {
+    const authToken=req?.headers?.authorization;
+    const authToken1=req?.headers
+    console.log(authToken1);
+    const token = authToken.split(' ')[1];
+    
+    console.log(authToken);
+   
+    const verify=jwt.verify(token,'secret');
+  
+    if (!verify?.email) {
+      return res.send("you are not authorize")
+      
+    }
+    req.user=verify.email;
+    next()
+  }
+
+
+  //================
+
+
+
+
 
 
 
@@ -26,60 +78,122 @@ async function run() {
   try {
     await client.connect();
 
-    // const productDB = client.db("productDB");
-    // const shoesCollection = productDB.collection("shoesCollection");
+    const productManagementDB = client.db("productManagementDB");
 
-    // product routes
-    // product routes create 
-    app.post("/shoes",async(req,res)=>{
-        const shoesData=req.body;
-        const result=await shoesCollection.insertOne(shoesData);
+
+    const productManagementUserCollection = productManagementDB.collection(
+      "productManagementUserCollection"
+    );
+
+
+    const productManagementCollection = productManagementDB.collection(
+        "productManagementProductDetails"
+      );
+
+    // userSelect: 
+
+
+    app.post("/user", async (req, res) => {
+        const user = req.body;
+        const token=createToken(user);
+  
+        const isUserExist = await productManagementUserCollection.findOne({
+          email: user?.email,
+        });
+  
+        if (isUserExist?._id) {
+        
+          return res.send({
+            status: "success",
+            message: "Log in success",
+            token,
+          });
+        }
+        await productManagementUserCollection.insertOne(user);
+         return res.send({token});
+      });
+
+
+      app.get("/user/:email", async (req, res) => {
+        const email = req.params.email;
+        const result = await productManagementUserCollection.findOne({ email });
         res.send(result);
+      });
 
-    })
-    // product routes gat All
-    app.get("/shoes",async(req,res)=>{
-       
-        const shoesData=shoesCollection.find();
-        const result=await shoesData.toArray();
+      app.patch("/user/:email", verifyToken, async (req, res) => {
+    //   app.patch("/user/:email",  async (req, res) => {
+        const email = req.params.email;
+        const userData = req.body;
+        const result = await productManagementUserCollection.updateOne(
+          { email },
+          { $set: userData },
+          { upsert: true }
+        );
         res.send(result);
+      });
 
-    })
-    // product routes gat one
-    app.get("/shoes/:id",async(req,res)=>{
-        const id=req.params.id;
-       
-        const shoesData=await shoesCollection.findOne({_id:new ObjectId(id)});
-        // const result=await shoesData.toArray();
-        res.send(shoesData);
+    //   =================================
 
-    })
-    // product routes patch update data
-    app.patch("/shoes/:id",async(req,res)=>{
-        const id=req.params.id;
-        const updatedData=req.body;
-       
-        const result=await shoesCollection.updateOne(
-            {_id:new ObjectId(id)},
-            {$set:updatedData}
+
+
+      //product create
+
+      app.post("/product",verifyToken, async (req, res) => {
+        const productData = req.body;
+        const result = await productManagementCollection.insertOne(productData);
+        res.send(result);
+      });
+
+
+         // product routes gat one
+         app.get("/product/get/:id", async (req, res) => {
+            const id = req.params.id;
+      
+            const productData = await productManagementCollection.findOne({
+              _id: new ObjectId(id),
+            });
+            // const result=await shoesData.toArray();
+            res.send(productData);
+          });
+  
+      //all product gate 
+      app.get("/products", async (req, res) => {
+        const productData = productManagementCollection.find();
+        const result = await productData.toArray();
+        res.send(result);
+      });
+
+
+       // product routes patch update data
+       app.patch("/product/edit/:id",verifyToken, async (req, res) => {
+        const id = req.params.id;
+        const updatedData = req.body;
+  
+        const result = await productManagementCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: updatedData }
         );
         // const result=await shoesData.toArray();
         res.send(result);
+      });
 
-    });
-    // product routes Delete data
-    app.delete("/shoes/:id",async(req,res)=>{
-        const id=req.params.id;
+
+      app.delete("/product/delete/:id",verifyToken,async (req, res) => {
+        const id = req.params.id;
         // const updatedData=req.body;
-       
-        const result=await shoesCollection.deleteOne(
-            {_id:new ObjectId(id)},
-            
-        );
+  
+        const result = await productManagementCollection.deleteOne({ _id: new ObjectId(id) });
         // const result=await shoesData.toArray();
         res.send(result);
+      });
 
-    });
+      /////////////
+
+
+
+
+
+    
 
 
 
